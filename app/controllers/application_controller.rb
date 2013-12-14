e7c037090e8c65238bbaef94dd51bce2e289e44a
Jon Wendt <jpwendt@asu.edu>
2013-12-13 22:49:52 -0700

    Updated bootstrap.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :puts_params
   before_filter :is_admin_user?
-
-  def puts_params
-    puts "====== PARAMS: #{params.inspect} ======"
-  end
 
   def is_admin_user?
     @is_admin = (current_user == User.find_by_email("jpwendt@asu.edu"))
