d9f18c43b13bed77bb3c279d422bacf8b42dbb98
Jon Wendt <jpwendt@asu.edu>
2015-09-05 21:29:08 -0700

    Can edit pictures.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,6 +6,20 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
   end
 
+  def edit
+    
+  end
+
+  def update
+    respond_to do |format|
+      if @picture and @picture.update_attributes(params[:picture])#.permit(:project_id, :title, :tags, :_destroy, post_contents_attributes: [:text, :id, content_attributes: []]))
+        format.html { redirect_to pictures_path }
+      else
+        format.html { redirect_to pictures_path }
+      end
+    end
+  end
+
   def add_from_urls
     Picture.add_from_urls(params[:urls].split(' '))
 
